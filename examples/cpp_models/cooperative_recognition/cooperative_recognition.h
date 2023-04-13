2199ec04854998340bf4407b58470c701f555c14
kuriatsu <kuriatsubayashi712@gmail.com>
2023-04-13 20:24:01 +0900

    add my model

---
+++ examples/cpp_models/cooperative_recognition/cooperative_recognition.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <despot/interface/pomdp.h>
+#include <despot/core/mdp.h>
+
+namespace despot {
+
+class CRState : public State {
+public:
+    int vehicle_position;
+    int 
+    CRState():
+    CRState(
+
+        
