45044062ff084d25bf26c5647dc9fa6accd57815
kuriatsu <kuriatsubayashi712@gmail.com>
2023-06-24 14:36:44 +0000

    sucess TraCI

--- examples/cpp_models/ras/sumo/test_traci.cpp
+++ examples/cpp_models/ras/sumo/test_traci.cpp
@@ -4,9 +4,14 @@
 using namespace libtraci;
 
 int main(int argc, char* argv[]) {
-    Simulation::start({"sumo", "-c", "straight_random.sumocfg"});
+    Simulation::start({"sumo-gui", "-c", "straight_random.sumocfg"});
     for (int i = 0; i < 3000; i++) {
         Simulation::step();
+        Person::add("ped_"+std::to_string(i), "E0", 0);
+        Person::setColor("ped_"+std::to_string(i), libsumo::TraCIColor(200, 0, 0));
+        Person::appendWalkingStage("ped_"+std::to_string(i), {"E0", "-E0"}, 0);
+        std::cout << Person::getPosition("ped_"+std::to_string(i)).getString() << std::endl;
     }
+
     Simulation::close();
 }
