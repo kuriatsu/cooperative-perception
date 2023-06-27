03590e3428da876cdc6dc424df9606807e26c1c9
kuriatsu <kuriatsubayashi712@gmail.com>
2023-06-27 04:25:44 +0000

    add vehicle

--- examples/cpp_models/ras/sumo/test_traci.cpp
+++ examples/cpp_models/ras/sumo/test_traci.cpp
@@ -7,8 +7,20 @@ int main(int argc, char* argv[]) {
     Simulation::start({"sumo-gui", "-c", "straight_random.sumocfg"});
     auto edge_list = Edge::getIDList();
     auto route_list = Route::getIDList(); 
-    Vehicle::add("ego_vehicle", route_list[0]);
+    if (Route::getIDList().empty()) {
+        Route::add("ego_vehicle_route", {"E0"});
+        std::cout << "add new route: " << Route::getIDList()[0] << std::endl;
+    }
+    
+    // for (auto &itr : Route::getIDList()){
+    //     std::cout << itr << std::endl;
+    // }
+
+    Vehicle::add("ego_vehicle", Route::getIDList()[0]);
     Vehicle::setColor("ego_vehicle", libsumo::TraCIColor(0, 200, 0));
+    Vehicle::setMaxSpeed("ego_vehicle", 50.0/3.6);
+    Vehicle::setAccel("ego_vehicle", 2.5*9.8);
+
     for (int i = 0; i < 3000; i++) {
         Simulation::step();
         Person::add("ped_"+std::to_string(i), "E0", 100);
