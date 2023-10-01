ef0b479b8f794ec4d3eb808d1bcfb6e80df0110e
kuriatsu <kuriatsubayashi712@gmail.com>
2023-10-01 23:34:01 +0900

    update gitignore

--- examples/cpp_models/ras/log/evaluation.sh
+++ examples/cpp_models/ras/log/evaluation.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/sh
 
-policies = (
+policies=(
     "POMDP"
     "MYOPIC"
     "EGOISTIC"
 )
 
-dencities = (
+dencities=(
     0.001
     0.005
     0.01
@@ -14,16 +14,16 @@ dencities = (
     0.03
     0.04
     0.05
-    0.06
-    0.07
-    0.08
-    0.09
-    0.1
+#    0.06
+#    0.07
+#    0.08
+#    0.09
+#    0.1
 )
 
 for policy in "${policies[@]}"; do
     for dencity in "${dencities[@]}"; do
-        for i in {1..10}; do
+        for i in {1..20}; do
             ../build/ras ${policy} ${dencity}
         done
     done
